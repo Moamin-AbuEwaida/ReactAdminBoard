c4d08fe22c9c7ed76bb98a16f36f4fd2a618e128
Moamin <moamin.salamah@gmail.com>
2025-05-26 15:12:53 +0200

    completed add modal

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -8,18 +8,26 @@ type Props = {
 }
 
 const Add = (props: Props) => {
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        // add new item
+        // axios.post(`/api/${slug}s`,{})
+    }
     return (
         <div className='add'>
             <div className="modal">
                 <span className="close" onClick={() => props.setOpen(false)}>x</span>
                 <h1>Add new {props.slug}</h1>
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     {props.columns.filter((item) => item.field !== 'id' && item.field !== 'img').map(column => (
                         <div className="item" key={column.headerName}>
                             <label>{column.headerName}</label>
                             <input type={column.type} placeholder={column.field} />
                         </div>
                     ))}
+                    <button>Send</button>
                 </form>
             </div>
         </div>
