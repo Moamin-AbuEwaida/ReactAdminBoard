9b87426da5bf9db5c24dab88d675b8ff1dc74b93
Moamin <moamin.salamah@gmail.com>
2025-05-26 14:25:31 +0200

    building the modal

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -1,9 +1,29 @@
+import type { GridColDef } from '@mui/x-data-grid';
 import './Add.scss'
 
-const AddUser = () => {
+type Props = {
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    columns: GridColDef[];
+    slug: string;
+}
+
+const Add = (props: Props) => {
     return (
-        <div className='add'>Addr</div>
+        <div className='add'>
+            <div className="modal">
+                <span className="close" onClick={() => props.setOpen(false)}>x</span>
+                <h1>Add new {props.slug}</h1>
+                <form action="">
+                    {props.columns.filter((item) => item.field !== 'id' && item.field !== 'img').map(column => (
+                        <div className="item" key={column.headerName}>
+                            <label>{column.headerName}</label>
+                            <input type={column.type} placeholder={column.field} />
+                        </div>
+                    ))}
+                </form>
+            </div>
+        </div>
     )
 }
 
-export default AddUser
+export default Add
